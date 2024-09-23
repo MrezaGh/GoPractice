08e5164ce3036f8fd9d79cff5e3aac3a4a9adaac
Mreza Gh <mrezagh.legend@gmail.com>
2024-09-23 10:27:22 +0330

    Solve ratelimit for free users per request excercise#3

--- 3-limit-service-time/main.go
+++ 3-limit-service-time/main.go
@@ -10,6 +10,8 @@
 
 package main
 
+import "time"
+
 // User defines the UserModel. Use this to check whether a User is a
 // Premium user or not
 type User struct {
@@ -21,7 +23,20 @@ type User struct {
 // HandleRequest runs the processes requested by users. Returns false
 // if process had to be killed
 func HandleRequest(process func(), u *User) bool {
-	process()
+	done := make(chan int)
+	go func() {
+		process()
+		close(done)
+	}()
+	throttle := time.Tick(10 * time.Second)
+	if u.IsPremium {
+		throttle = make(chan time.Time)
+	}
+	select {
+	case <-done:
+	case <-throttle:
+		return false
+	}
 	return true
 }
 
